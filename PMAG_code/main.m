46d74b1c937963fb752d9a7ac1f9a7f06d27a965
caccode <648667527@qq.com>
2025-04-27 19:11:50 +0800

    Add files via upload

--- PMAG_code/main.m
+++ PMAG_code/main.m
@@ -12,7 +12,7 @@ for projectionDim= [10:10:100]
     for r = [1.2,1.4,1.6,1.8,2,4,6,8,10,12]
         for cnt=1:20
             count=count+1;
-            [result,alpha,runtime,obj] = run_ours(ds,projectionDim,r);
+            [result,alpha,runtime,obj] = run_PMAG(ds,projectionDim,r);
             fprintf(fid, 'ACC = %0.4f NMI = %0.4f Purity = %0.4f RunTime= %f r = %0.1f projectionDim=%d \r\n',result(1:3),runtime,r,projectionDim);
         end
 
